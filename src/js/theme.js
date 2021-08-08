5f23eaa0e0fbdd488434464f31b5a67208681451
Alexey Zyukov <aalex64@ukr.net>
2021-08-08 23:47:25 +0300

    Update theme.js

--- src/js/theme.js
+++ src/js/theme.js
@@ -9,10 +9,19 @@ console.log(bodyTheme.classList);
 console.log(bodyTheme.classList.value);
 
 function onClickChangeTheme(event) {
-    console.log(event.target);
-    document.body.classList.add('.body.dark-theme');
+    // console.log(event.target);
+    if (bodyTheme.classList.contains('light-theme')) {
+        console.log(bodyTheme.classList.contains('light-theme'))
+        bodyTheme.classList.remove('light-theme');
+        bodyTheme.classList.add('dark-theme');
+    };
+    
+    if (bodyTheme.classList.contains('dark-theme')) {
+        bodyTheme.classList.replace('dark-theme', 'light-theme');
+    };
     // document.body.style.backgroundColor = 'green';
     // if (body.classList.contains ('.light-theme') {
     //     body.classList.replace('light-theme', 'dark-theme');
     // }
 };
+console.log(bodyTheme.classList.value);
