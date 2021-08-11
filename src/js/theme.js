bf4dff526a6872158098c65a9c87b9964c059370
Alexey Zyukov <aalex64@ukr.net>
2021-08-11 22:35:01 +0300

    home work done

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,27 +1,32 @@
 import '../styles.css';
 import '../sass/main.scss';
 
-const themeSwitchControl = document.querySelector('.theme-switch__control');
-themeSwitchControl.addEventListener('click', onClickChangeTheme);
 
-const bodyTheme = document.querySelector('body');
-console.log(bodyTheme.classList);
-console.log(bodyTheme.classList.value);
+const themeSwitchControl = document.querySelector('#theme-switch-toggle');
+console.log(themeSwitchControl);
+themeSwitchControl.addEventListener('change', onClickChangeTheme);
+themeSwitchControl.addEventListener('change', onClickChangeLocalStorage);
+restoreTheme();
 
-function onClickChangeTheme(event) {
-    // console.log(event.target);
-    if (bodyTheme.classList.contains('light-theme')) {
-        console.log(bodyTheme.classList.contains('light-theme'))
-        bodyTheme.classList.remove('light-theme');
-        bodyTheme.classList.add('dark-theme');
-    };
-    
-    if (bodyTheme.classList.contains('dark-theme')) {
-        bodyTheme.classList.replace('dark-theme', 'light-theme');
-    };
-    // document.body.style.backgroundColor = 'green';
-    // if (body.classList.contains ('.light-theme') {
-    //     body.classList.replace('light-theme', 'dark-theme');
-    // }
+
+function onClickChangeTheme() {
+    document.body.classList.toggle('dark-theme');
 };
-console.log(bodyTheme.classList.value);
+
+function onClickChangeLocalStorage() {
+    const change = themeSwitchControl.checked
+    if (change) {
+        localStorage.setItem('theme', 'dark-theme')
+    } else {
+        localStorage.removeItem('theme');
+    }
+}
+
+function restoreTheme() {
+    const savedTheme = localStorage.getItem('theme')
+    if (savedTheme === 'dark-theme') {
+        console.log(savedTheme);
+        document.body.classList.add('dark-theme');
+        themeSwitchControl.checked = true;
+    }
+}
